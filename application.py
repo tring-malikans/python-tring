0ca36d793f0fff5a59292cc1b8e5417071b2937f
Sufiyan <sufiyan7697@gmail.com>
2021-04-02 15:03:01 +0530

    added new pairs

--- application.py
+++ application.py
@@ -25,8 +25,8 @@ db = mongo1['test-database']
 # # coins=["BTC","SUSHI","DOGE",'ADA','EOS','XRP','VET','TRX','LINK','THETA']
 # coins=["BTC","XRP","DOGE",'ADA','THETA','']
 
-# coins=["BTC","XRP","DOGE","ADA","THETA"]
-# pairs=["BUSD"]
+coins=["BTC","XRP","DOGE","ADA","THETA"]
+pairs=["BUSD"]
 
 
 
